26984cd5da63d6af46c8cdda95548c4a03cc0cd4
dmitry <dmitry@dmitry-pc>
2022-05-03 16:07:00 +0700

    first commit

---
+++ src/main/java/com/virginstudio/coffee_weather/WeatherInfo.java
@@ -0,0 +1,25 @@
+package com.virginstudio.coffee_weather;
+
+import java.util.List;
+
+public class WeatherInfo {
+    Coordinates coord;
+    List<WeatherIdentificator> weather;;
+    String base;
+    MainWeatherInfo main;
+    float visibility;
+    Wind wind;
+    Clouds clouds;
+    int dt;
+    SystematicInfo sys;
+    int timezone;
+    int id;
+    String name;
+    int code;
+    
+    
+    public WeatherInfo () {
+        
+    }
+    
+}
